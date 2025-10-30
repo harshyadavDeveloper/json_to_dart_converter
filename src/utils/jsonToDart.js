32eff2b799a1fb3524a9a11a43a945f2d2c09b43
harshyadavDeveloper <harshyadav5939@gmail.com>
2025-10-30 15:21:36 +0530

    implemented code to return the nested json model correctly

--- src/utils/jsonToDart.js
+++ src/utils/jsonToDart.js
@@ -1,44 +1,113 @@
-export function jsonToDart(jsonString, className = "MyModel") {
+export function jsonToDart(jsonString, rootClassName = "MyModel") {
+  let json;
   try {
-    const json = JSON.parse(jsonString);
+    json = JSON.parse(jsonString);
+  } catch (error) {
+    throw new Error("Invalid JSON", error);
+  }
 
-    const fields = Object.keys(json)
-      .map((key) => {
-        const type = typeof json[key];
-        let dartType = "dynamic";
-        if (type === "string") dartType = "String";
-        else if (type === "number") dartType = "num";
-        else if (type === "boolean") dartType = "bool";
-        else if (Array.isArray(json[key])) dartType = "List<dynamic>";
-        else if (type === "object") dartType = "Map<String, dynamic>";
-        return `  final ${dartType} ${key};`;
-      })
-      .join("\n");
+  const classDefs = [];
 
-    return `
+  const toPascalCase = (str) =>
+    str
+      .replace(/([-_][a-z])/gi, (s) =>
+        s.toUpperCase().replace("-", "").replace("_", "")
+      )
+      .replace(/^\w/, (c) => c.toUpperCase());
+
+  function generateClass(name, obj) {
+    const className = toPascalCase(name);
+    const fields = [];
+    const fromJsonLines = [];
+    const toJsonLines = [];
+
+    for (const key in obj) {
+      const value = obj[key];
+      let fieldType = "dynamic";
+      let assignFromJson = "";
+      let assignToJson = "";
+
+      if (Array.isArray(value)) {
+        if (
+          value.length > 0 &&
+          typeof value[0] === "object" &&
+          !Array.isArray(value[0])
+        ) {
+          const nestedClassName = toPascalCase(key.slice(0, -1) || key);
+          generateClass(nestedClassName, value[0]);
+          fieldType = `List<${nestedClassName}>?`;
+          assignFromJson = `
+    if (json['${key}'] != null) {
+      ${key} = <${nestedClassName}>[];
+      json['${key}'].forEach((v) {
+        ${key}!.add(${nestedClassName}.fromJson(v));
+      });
+    }`;
+          assignToJson = `
+    if (${key} != null) {
+      data['${key}'] = ${key}!.map((v) => v.toJson()).toList();
+    }`;
+        } else {
+          fieldType = "List<dynamic>?";
+          assignFromJson = `${key} = json['${key}'];`;
+          assignToJson = `data['${key}'] = ${key};`;
+        }
+      } else if (typeof value === "object" && value !== null) {
+        const nestedClassName = toPascalCase(key);
+        generateClass(nestedClassName, value);
+        fieldType = `${nestedClassName}?`;
+        assignFromJson = `${key} = json['${key}'] != null ? ${nestedClassName}.fromJson(json['${key}']) : null;`;
+        assignToJson = `
+    if (${key} != null) {
+      data['${key}'] = ${key}!.toJson();
+    }`;
+      } else if (typeof value === "string") {
+        fieldType = "String?";
+        assignFromJson = `${key} = json['${key}'];`;
+        assignToJson = `data['${key}'] = ${key};`;
+      } else if (typeof value === "number") {
+        fieldType = "int?";
+        assignFromJson = `${key} = json['${key}'];`;
+        assignToJson = `data['${key}'] = ${key};`;
+      } else if (typeof value === "boolean") {
+        fieldType = "bool?";
+        assignFromJson = `${key} = json['${key}'];`;
+        assignToJson = `data['${key}'] = ${key};`;
+      } else {
+        fieldType = "dynamic";
+        assignFromJson = `${key} = json['${key}'];`;
+        assignToJson = `data['${key}'] = ${key};`;
+      }
+
+      fields.push(`  ${fieldType} ${key};`);
+      fromJsonLines.push(`    ${assignFromJson}`);
+      toJsonLines.push(`    ${assignToJson}`);
+    }
+
+    const classDef = `
 class ${className} {
-${fields}
+${fields.join("\n")}
 
-  ${className}({
-${Object.keys(json)
-  .map((key) => `    required this.${key},`)
-  .join("\n")}
-  });
+  ${className}({${Object.keys(obj)
+      .map((k) => `this.${k}`)
+      .join(", ")}});
 
-  factory ${className}.fromJson(Map<String, dynamic> json) => ${className}(
-${Object.keys(json)
-  .map((key) => `        ${key}: json['${key}'],`)
-  .join("\n")}
-  );
+  ${className}.fromJson(Map<String, dynamic> json) {
+${fromJsonLines.join("\n")}
+  }
 
-  Map<String, dynamic> toJson() => {
-${Object.keys(json)
-  .map((key) => `        '${key}': ${key},`)
-  .join("\n")}
-  };
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+${toJsonLines.join("\n")}
+    return data;
+  }
 }
 `;
-  } catch (err) {
-    return `❌ Invalid JSON. Please check your input. ${err.message}`;
+    classDefs.push(classDef);
   }
+
+  const rootName = toPascalCase(rootClassName);
+  generateClass(rootName, json);
+
+  return classDefs.reverse().join("\n");
 }
